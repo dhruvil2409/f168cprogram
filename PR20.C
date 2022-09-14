d564942e3cc67e80dbd16e3926504aa763cc3016
dhruvil2409 <pateldhruvil589@gmail.com>
2022-09-14 12:50:38 +0530

    file uploaded

---
+++ PR20.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=5,b=10;
+clrscr();
+++a;
+printf(" the value of x is :%d\n",a);
+--b;
+printf(" the value of y is :%d\n",b);
+getch();
+}
